9804cb6613edd6024692f15c766f0d1a0e746227
Sneyder Barreto <hello@sneyder.dev>
2024-07-18 19:44:50 -0500

    fix(types): add `ViewTransition` API type definitions

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,21 @@
 /// <reference types="astro/client" />
+
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ViewTransition) */
+interface ViewTransition {
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ViewTransition/finished) */
+  readonly finished: Promise<undefined>;
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ViewTransition/ready) */
+  readonly ready: Promise<undefined>;
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ViewTransition/updateCallbackDone) */
+  readonly updateCallbackDone: Promise<undefined>;
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/ViewTransition/skipTransition) */
+  skipTransition(): void;
+}
+
+interface UpdateCallback {
+  (): any;
+}
+
+interface Document {
+  startViewTransition(callbackOptions?: UpdateCallback): ViewTransition;
+}
