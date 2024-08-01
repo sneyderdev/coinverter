b9ce38ef62fa886c0a3c87f5d7a32160248a4294
Sneyder Barreto <hello@sneyder.dev>
2024-08-01 15:23:48 -0500

    feat: show error messages if any SSR component fail to fetch data

--- src/scripts/historical.ts
+++ src/scripts/historical.ts
@@ -2,16 +2,15 @@ import invariant from "tiny-invariant";
 import dayjs from "dayjs";
 
 import { $historical, $converter } from "@/store";
-
-import type { Historical } from "@/types/app";
+import { getHistorical } from "@/lib/utils";
 
 import "./requestStatus";
 
 const historicalEl = document.getElementById("historical");
 invariant(historicalEl, "Historical element not found");
-invariant(historicalEl.dataset.historical, "Historical data not found");
+invariant(historicalEl.dataset.data, "Historical data not found");
 
-$historical.set(JSON.parse(historicalEl.dataset.historical) as Historical);
+$historical.set(getHistorical(JSON.parse(historicalEl.dataset.data), "COP"));
 
 const chartDescriptionEl = document.getElementById("chart_description");
 invariant(chartDescriptionEl, "Chart description element not found");
